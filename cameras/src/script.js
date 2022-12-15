c551ce47c619e33dc21fbf47f2d6f36218c0982f
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2022-12-15 16:32:03 +0000

    add add orbit controls and damping, add updates to camera to move left to right or move around the cube

--- cameras/src/script.js
+++ cameras/src/script.js
@@ -1,5 +1,16 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+//Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
 
 /**
  * Base
@@ -24,27 +35,31 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   sizes.width / sizes.height,
-//   0.1,
-//   100
-// );
-const aspectRatio = sizes.width / sizes.height;
-console.log(aspectRatio);
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+// const aspectRatio = sizes.width / sizes.height;
+// console.log(aspectRatio);
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -59,7 +74,22 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  //   mesh.rotation.y = elapsedTime;
+
+  //Update camera
+  //camera moves from left to right
+  //   camera.position.x = cursor.x * 10;
+  //   camera.position.y = cursor.y * 10;
+
+  //make camera move around the cube
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3; // using Pi * 2 does a full rotation once
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   //   camera.lookAt(new THREE.Vector3()); //or
+  //   camera.lookAt(mesh.position);
+
+  //Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
