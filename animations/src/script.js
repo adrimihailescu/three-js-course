132a6070ad2698054c60e94c5be7bc2454345069
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2022-12-15 12:50:11 +0000

    add Clock variable and use built in methos getElapsedTime to rotate the object

--- animations/src/script.js
+++ animations/src/script.js
@@ -30,19 +30,28 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-//Time
-let time = Date.now();
+// //Time
+// let time = Date.now();
+
+//Clock
+const clock = new THREE.Clock();
 
 //Animations
 const tick = () => {
-  //Time
-  const currentTime = Date.now();
-  const deltaTime = currentTime - time;
-  time = currentTime;
+  //Clock
+  const elapsedTime = clock.getElapsedTime();
+  //   //Time
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
 
-  console.log(deltaTime);
+  //   console.log(deltaTime);
+
   //Update objects
-  mesh.rotation.y += 0.001 * deltaTime;
+  //   mesh.rotation.y += 0.001 * deltaTime;
+  //   mesh.rotation.y = elapsedTime * Math.PI * 2;
+  mesh.position.y = Math.sin(elapsedTime); // rotate cube
+  mesh.position.x = Math.cos(elapsedTime); // rotate cube
 
   //Render
   renderer.render(scene, camera);
