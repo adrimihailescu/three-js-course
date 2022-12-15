98c680124167087b2bdb934ca62d25bbdc2fb83b
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2022-12-15 17:41:43 +0000

    add logic for full screen and logic for Safari in particular

--- fullscreen-and-resize/src/script.js
+++ fullscreen-and-resize/src/script.js
@@ -43,6 +43,25 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+//fullscreen
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  //for Safari browser
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
 /**
  * Camera
  */
