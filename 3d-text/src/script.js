9fb5c1fc358626434698d0694980854136c4533a
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-01-27 15:13:38 +0000

    add material and meshes for donuts and text

--- 3d-text/src/script.js
+++ 3d-text/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
 /**
  * Base
@@ -15,10 +16,17 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// /**
+//  * Axis Helper - helps seeing the center of the text and to calculate it
+//  */
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
 
 /**
  * Fonts
@@ -26,18 +34,65 @@ const textureLoader = new THREE.TextureLoader();
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  console.log("loaded");
+  const textGeometry = new TextGeometry("Hello Adriana", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+
+  //how to center the the text(the mesh) using a box
+  //   textGeometry.computeBoundingBox();
+  //   //   console.log(textGeometry.boundingBox);
+  //   textGeometry.translate(
+  //     -(textGeometry.boundingBox?.max.x - 0.02) / 2,
+  //     -(textGeometry.boundingBox?.max.y - 0.02) / 2,
+  //     -(textGeometry.boundingBox?.max.z - 0.03) / 2
+  //   );
+
+  //easiest way to center the text
+  textGeometry.center();
+
+  const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  //   textMaterial.wireframe = true;
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
 });
+
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+
+for (let i = 0; i < 100; i++) {
+  const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+
+  donut.position.x = (Math.random() - 0.5) * 10;
+  donut.position.y = (Math.random() - 0.5) * 10;
+  donut.position.z = (Math.random() - 0.5) * 10;
+
+  donut.rotation.x = Math.random() * Math.PI;
+  donut.rotation.y = Math.random() * Math.PI;
+
+  const scale = Math.random();
+  donut.scale.x = scale;
+  donut.scale.y = scale;
+  donut.scale.z = scale;
+  scene.add(donut);
+}
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial()
+// );
 
-scene.add(cube);
+// scene.add(cube);
 
 /**
  * Sizes
