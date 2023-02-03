2a302ce145cb2f6ed231b1f1e09dd1eee755f134
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-02-03 16:19:24 +0000

    add event listener on scroll and position objects

--- scroll-based-animations/src/script.js
+++ scroll-based-animations/src/script.js
@@ -10,7 +10,9 @@ const parameters = {
   materialColor: "#ffeded",
 };
 
-gui.addColor(parameters, "materialColor");
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor);
+});
 
 /**
  * Base
@@ -33,10 +35,18 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
+//Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
+// console.log(gradientTexture);
+//Material
 const material = new THREE.MeshToonMaterial({
   color: parameters.materialColor,
+  gradientMap: gradientTexture,
 });
 //Meshes
+const objectsDistance = 4;
 const mesh1 = new THREE.Mesh(
   new THREE.TorusGeometry(1, 0.4, 16, 60),
   //   new THREE.MeshBasicMaterial({ color: "#ff0000" })
@@ -52,7 +62,17 @@ const mesh3 = new THREE.Mesh(
   //   new THREE.MeshBasicMaterial({ color: "#ff0000" })
   material
 );
+
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
+
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
 scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
 /**
  * Light
@@ -107,12 +127,31 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+  //   console.log(scrollY);
+});
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  //Animate camera
+  camera.position.y = (-scrollY / sizes.height) * objectsDistance;
+
+  //Animate meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x = elapsedTime * 0.1;
+    mesh.rotation.y = elapsedTime * 0.12;
+  }
 
   // Render
   renderer.render(scene, camera);
