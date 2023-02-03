d80ef97b3e7cc70158a12b27ab458c8ee575981e
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-02-03 17:31:41 +0000

    add gsap to animate each object once each section is reached

--- scroll-based-animations/src/script.js
+++ scroll-based-animations/src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import * as dat from "lil-gui";
-
+import gsap from "gsap";
 /**
  * Debug
  */
@@ -170,10 +170,27 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Scroll
  */
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener("scroll", () => {
   scrollY = window.scrollY;
   //   console.log(scrollY);
+
+  const newSection = Math.round(scrollY / sizes.height);
+  //   console.log(newSection);
+  if (newSection != currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
+
+    // console.log("changed", currentSection);
+  }
 });
 
 /**
@@ -215,8 +232,8 @@ const tick = () => {
 
   //Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.12;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // Render
