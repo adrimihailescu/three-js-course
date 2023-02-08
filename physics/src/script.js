a5bb0cd47b622b2320265c44a39b4b89c483f80b
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-02-08 13:11:31 +0000

    add create sphere button and add to gui

--- physics/src/script.js
+++ physics/src/script.js
@@ -9,6 +9,7 @@ import CANNON from "cannon";
  * Debug
  */
 const gui = new dat.GUI();
+const debugObject = {};
 
 /**
  * Base
@@ -212,7 +213,15 @@ const createSphere = (radius, position) => {
     body,
   });
 };
-createSphere(0.5, { x: 0, y: 3, z: 0 });
+debugObject.createSphere = () => {
+  createSphere(Math.random() * 0.5, {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+
+gui.add(debugObject, "createSphere");
 
 /**
  * Animate
