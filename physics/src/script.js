9b0fe6f672f61c998cd706c0261980f514747210
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-02-08 12:49:45 +0000

    fixed bug to make the sphere jump

--- physics/src/script.js
+++ physics/src/script.js
@@ -39,21 +39,22 @@ const environmentMapTexture = cubeTextureLoader.load([
  */
 
 const world = new CANNON.World();
-world.gravity.set(0, -0.98, 0);
+world.gravity.set(0, -9.82, 0);
 
 //Materials
-const concreteMaterial = new CANNON.Material("concrete");
-const plasticMaterial = new CANNON.Material("plastic");
+const defaultMaterial = new CANNON.Material("default");
+// const defaultContactMaterial = new CANNON.Material("defaultMaterial");
 
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-  concreteMaterial,
-  plasticMaterial,
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
   {
-    friction: 0.5,
+    friction: 0.1,
     restitution: 0.7,
   }
 );
-world.addContactMaterial(concretePlasticContactMaterial);
+// world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
 
 //Sphere
 // const sphereShape = new CANNON.Sphere(0.5); // 0.5 is the radius of the sphere
@@ -74,7 +75,7 @@ world.addContactMaterial(concretePlasticContactMaterial);
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
 floorBody.mass = 0; // this means that the floor will have a fix position
-floorBody.material = concreteMaterial;
+floorBody.material = defaultMaterial;
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 world.addBody(floorBody);
