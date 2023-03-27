91187b5054c4ee06c763fdbbd5257fb3dbd7a371
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-03-27 17:18:32 +0100

    add texture, add uv attribute and control elevation

--- shaders/src/script.js
+++ shaders/src/script.js
@@ -20,6 +20,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const flagTexture = textureLoader.load("/textures/flag-french.jpg");
 
 /**
  * Test mesh
@@ -45,6 +46,9 @@ const material = new THREE.RawShaderMaterial({
   uniforms: {
     uFrequency: { value: new THREE.Vector2(10, 5) },
     uTime: { value: 0 },
+    uColor: { value: new THREE.Color("orange") },
+    //send the texture as a uniform
+    uTexture: { value: flagTexture },
   },
 });
 
--- shaders/src/shaders/test/vertex.glsl
+++ shaders/src/shaders/test/vertex.glsl
@@ -5,21 +5,34 @@ uniform vec2 uFrequency;
 uniform float uTime;
 
 attribute vec3 position;
-attribute float aRandom;
+//retrieve the attribute uv in the vertex shader
+attribute vec2 uv;
+
+//send data from the vertex to the fragment shader
+varying vec2 vUv;
+varying float vElevation;
+
+// attribute float aRandom;
 
 // varying float vRandom;
 
 void main()
 {
 vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+//elevation
+float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+modelPosition.z += elevation;
 // modelPosition.z += aRandom * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     
     gl_Position = projectedPosition;
+    vUv = uv;
+    vElevation = elevation;
 
     // vRandom = aRandom;
 }
