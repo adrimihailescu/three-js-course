234a2d4a52aee0a76795602cfb215f0a4c4d4fb7
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-03-29 16:36:51 +0100

    send uv coordinates to the fragment and add first pattern- change the x and the y values with vUv

--- shader-patterns/src/shaders/test/fragment.glsl
+++ shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,10 @@
+//retrieve the vUv varying
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    //replace the x and y with vUv
+    // gl_FragColor = vec4(vUv.x, vUv.y, 0.5, 1.0);
+    //
+    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
 }
--- shader-patterns/src/shaders/test/vertex.glsl
+++ shader-patterns/src/shaders/test/vertex.glsl
@@ -1,4 +1,10 @@
+//send this value to the fragment
+varying vec2 vUv;
+
+
 void main()
 {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    vUv = uv;
 }
