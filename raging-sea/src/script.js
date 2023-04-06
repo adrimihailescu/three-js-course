c4d180222cfc23281d3d00a8af898fba80848c72
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-04-06 15:31:27 +0100

    add uniforms

--- raging-sea/src/script.js
+++ raging-sea/src/script.js
@@ -26,8 +26,32 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
 const waterMaterial = new THREE.ShaderMaterial({
   vertexShader: waterVertexShader,
   fragmentShader: waterFragmentShader,
-  //   side: THREE.DoubleSide,
+  uniforms: {
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+  },
+  side: THREE.DoubleSide,
 });
+
+//Debug
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uBigWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyX");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyY");
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
