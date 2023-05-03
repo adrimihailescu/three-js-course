ea826d71deb913d5fe53f6e81ab36e3d9fe8c4d6
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-03 18:02:24 +0100

    add meshes for a plane, a sphera and a cube,add useFrame hook to rotate cube using useRef

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,9 +1,34 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 export default function Experience() {
+  const cubeRef = useRef();
+  useFrame(() => {
+    cubeRef.current.rotation.y += 0.01;
+  });
+
   return (
     <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color="salmon" />
+      </mesh>
+
+      <mesh
+        ref={cubeRef}
+        scale={1.5}
+        position-x={2}
+        rotation-y={Math.PI * 0.25}
+      >
+        <boxGeometry scale={1.5} />
+        <meshBasicMaterial args={[{ color: "red" }]} />
+        {/* <torusKnotGeometry />
+        <meshNormalMaterial /> */}
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
       </mesh>
     </>
   );
