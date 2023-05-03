3707a40cd046714660f9db68de2e014eca4d6dd8
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-03 16:09:23 +0100

    add useRef hook to change button background color, practice purpose

--- first-react-app/src/Clicker.jsx
+++ first-react-app/src/Clicker.jsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function CLicker({ increment, keyName, color }) {
   const [count, setCount] = useState(
     parseInt(localStorage.getItem(keyName) ?? 0)
   ); //set the state with what is saved in localStorage
 
+  //first create a reference then associate it to the targeted element with ref attribute
+  const buttonRef = useRef();
+  // console.log(buttonRef);
+
   //retrieve value from the first render
   useEffect(() => {
     // const savedCount = parseInt(localStorage.getItem("count") ?? 0);
     // // console.log(savedCount);
     // setCount(savedCount);
+
+    buttonRef.current.style.backgroundColor = "papayawhip";
     return () => {
       localStorage.removeItem(keyName); //remove from localStorage
     };
@@ -27,7 +33,9 @@ export default function CLicker({ increment, keyName, color }) {
   return (
     <div>
       <div style={{ color }}>CLick counts {count}</div>
-      <button onClick={buttonCLick}>CLick me</button>
+      <button onClick={buttonCLick} ref={buttonRef}>
+        CLick me
+      </button>
     </div>
   );
 }
