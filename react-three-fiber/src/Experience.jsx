fc722b194a41f2ca52f751e1942196dec99f4a98
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-04 15:54:47 +0100

    add orbit controls to control the camera

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,32 +1,49 @@
-import { useFrame } from "@react-three/fiber";
+import { useThree, extend, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+extend({ OrbitControls });
 export default function Experience() {
+  //we use ref so we can target the element we want to rotate, add it to the mesh and add the rotation in the useFrame hook
   const cubeRef = useRef();
-  useFrame(() => {
-    cubeRef.current.rotation.y += 0.01;
+  const groupRef = useRef();
+
+  const { camera, gl } = useThree();
+  // console.log(camera, gl);
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta; //this will rotate the group
   });
 
   return (
     <>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="salmon" />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
+
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="salmon" />
+        </mesh>
+
+        <mesh
+          ref={cubeRef}
+          scale={1.5}
+          position-x={2}
+          rotation-y={Math.PI * 0.25}
+        >
+          <boxGeometry scale={1.5} />
+          <meshBasicMaterial args={[{ color: "red" }]} />
+          {/* <torusKnotGeometry />
+        <meshNormalMaterial /> */}
+        </mesh>
+      </group>
 
       <mesh
-        ref={cubeRef}
-        scale={1.5}
-        position-x={2}
-        rotation-y={Math.PI * 0.25}
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+        // ref={planeRef}
       >
-        <boxGeometry scale={1.5} />
-        <meshBasicMaterial args={[{ color: "red" }]} />
-        {/* <torusKnotGeometry />
-        <meshNormalMaterial /> */}
-      </mesh>
-
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
       </mesh>
