d9fd64070f707e79e52bbaa477b38c331190baa0
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-05 15:00:19 +0100

    add useMemo, useRef and useEffect hooks for saving positions, get access to the geometry and calculate normals

--- react-three-fiber/src/CustomObject.jsx
+++ react-three-fiber/src/CustomObject.jsx
@@ -1,14 +1,28 @@
 import * as THREE from "three";
+import { useMemo, useRef, useEffect } from "react";
 
 export default function CustomObject() {
-  const verticesCount = 10 * 3;
-  const positions = new Float32Array(verticesCount * 3);
+  //calculate normals we use useRef hook to get the reference of the geometry
+  const geometryRef = useRef();
 
-  for (let i = 0; i < verticesCount * 3; i++)
-    positions[i] = (Math.random() - 0.5) * 3;
+  const verticesCount = 10 * 3;
+
+  //useMemo hook to save the calculation of the positions so it does not get calculated on each render
+  const positions = useMemo(() => {
+    const positions = new Float32Array(verticesCount * 3);
+
+    for (let i = 0; i < verticesCount * 3; i++)
+      positions[i] = (Math.random() - 0.5) * 3;
+    return positions;
+  }, []);
+
+  //useEffect to get the normals only after the first render happens
+  useEffect(() => {
+    geometryRef.current.computeVertexNormals();
+  }, []);
   return (
     <mesh>
-      <bufferGeometry>
+      <bufferGeometry ref={geometryRef}>
         <bufferAttribute
           attach="attributes-position"
           count={verticesCount}
@@ -16,7 +30,7 @@ export default function CustomObject() {
           array={positions}
         />
       </bufferGeometry>
-      <meshBasicMaterial color="purple" side={THREE.DoubleSide} />
+      <meshStandardMaterial color="purple" side={THREE.DoubleSide} />
     </mesh>
   );
 }
