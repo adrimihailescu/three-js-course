e2081f73227c69b784387aaa38faf5e2f131da15
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-12 17:07:28 +0100

    add ways of adding/changing the background color

--- environment-and-staging-with-r3f/src/index.jsx
+++ environment-and-staging-with-r3f/src/index.jsx
@@ -1,19 +1,46 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import Experience from "./Experience.jsx";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+//ways of changing the background
+/**
+ *
+ * with setClearColor from gl
+ */
+// const created = ({ gl }) => {
+//   //   console.log("created");
+//   gl.setClearColor("#ff0000");
+// };
+
+/**
+ *
+ * with the scene background
+ */
+// const created = ({ scene }) => {
+//   //   console.log("created");
+//   scene.background = new THREE.Color("#ff0000");
+// };
+
+/**
+ * with R3F color
+ */
+// create a color tag inside the canvas
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
-)
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+    // onCreated={created}
+  >
+    <color args={["red"]} attach="background" />
+    <Experience />
+  </Canvas>
+);
