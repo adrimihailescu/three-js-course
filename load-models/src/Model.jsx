05fe6cc11bec12e9fb59fb9235d5c1cf11a3fa98
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-16 15:47:24 +0100

    add the model with useGLTF hook from drei

--- load-models/src/Model.jsx
+++ load-models/src/Model.jsx
@@ -1,18 +1,16 @@
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { useGLTF } from "@react-three/drei";
+// import { useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 export default function Model() {
-  const model = useLoader(
-    GLTFLoader,
-    "./FlightHelmet/glTF/FlightHelmet.gltf",
-    (loader) => {
-      // console.log(loader);
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("./draco/");
-      loader.setDRACOLoader(dracoLoader);
-    }
-  );
-  console.log(model);
-  return <primitive object={model.scene} scale={5} position-y={-1} />;
+  // const model = useLoader(GLTFLoader, "./hamburger.glb", (loader) => {
+  //   // console.log(loader);
+  //   const dracoLoader = new DRACOLoader();
+  //   dracoLoader.setDecoderPath("./draco/");
+  //   loader.setDRACOLoader(dracoLoader);
+  // });
+  // console.log(model);
+  const model = useGLTF("/hamburger.glb");
+  return <primitive object={model.scene} scale={0.35} />;
 }
