d22c33d23a3cef217a5b58c9178be19a50530b94
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-17 13:11:31 +0100

    remove cube from scene, add texture and load model for portal

--- portal-scene-r3f/src/Experience.jsx
+++ portal-scene-r3f/src/Experience.jsx
@@ -1,15 +1,19 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture, Center } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
-
-        <OrbitControls makeDefault />
-
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
+export default function Experience() {
+  const { nodes } = useGLTF("./model/portal.glb");
+  const bakedTexture = useTexture("./model/baked.jpg");
+  bakedTexture.flipY = false;
+  console.log(nodes);
+  return (
+    <>
+      <color args={["#030202"]} attach="background" />
+      <OrbitControls makeDefault />
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
         </mesh>
-
+      </Center>
     </>
+  );
 }
