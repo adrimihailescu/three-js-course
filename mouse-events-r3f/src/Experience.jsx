d8a9513e95e4026503bd818160339b7cd6a739cf
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-17 16:51:36 +0100

    add gltf loader to load the burger and add on click to the primitive and stop propagation

--- mouse-events-r3f/src/Experience.jsx
+++ mouse-events-r3f/src/Experience.jsx
@@ -1,9 +1,11 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useRef } from "react";
+import Hamburger from "../../load-models/src/Hamburger";
 
 export default function Experience() {
   const cube = useRef();
+  const hamburger = useGLTF("./hamburger.glb");
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -52,6 +54,15 @@ export default function Experience() {
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
+      <primitive
+        object={hamburger.scene}
+        scale={0.25}
+        position-y={1}
+        onClick={(event) => {
+          console.log(event.object.name);
+          event.stopPropagation();
+        }}
+      />
     </>
   );
 }
