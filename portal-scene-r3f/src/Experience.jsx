0183f8820fcfc87106b2b5283e1074cb1dbf5512
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-17 15:39:17 +0100

    add two options for adding a custom shader material

--- portal-scene-r3f/src/Experience.jsx
+++ portal-scene-r3f/src/Experience.jsx
@@ -4,13 +4,29 @@ import {
   useTexture,
   Center,
   Sparkles,
+  shaderMaterial,
 } from "@react-three/drei";
+import { extend } from "@react-three/fiber";
+import * as THREE from "three";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+// console.log(portalFragmentShader);
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#ffffff"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
+extend({ PortalMaterial });
 
 export default function Experience() {
   const { nodes } = useGLTF("./model/portal.glb");
   const bakedTexture = useTexture("./model/baked.jpg");
   bakedTexture.flipY = false;
-  console.log(nodes);
   return (
     <>
       <color args={["#030202"]} attach="background" />
@@ -41,8 +57,32 @@ export default function Experience() {
           geometry={nodes.portalLight.geometry}
           position={nodes.portalLight.position}
           rotation={nodes.portalLight.rotation}
-        ></mesh>
-        <Sparkles size={6} position-y={1} scale={(4, 2, 4)} />
+        >
+          {/**
+           * tag made available through extend(), second option of adding a custom shader material
+           */}
+          <portalMaterial />
+          {/**
+           * first option for adding a shader material
+           */}
+          {/* <shaderMaterial
+            vertexShader={portalVertexShader}
+            fragmentShader={portalFragmentShader}
+            uniforms={{
+              uTime: { value: 0 },
+              uColorStart: { value: new THREE.Color("#ffffff") },
+              uColorEnd: { value: new THREE.Color("#000000") },
+            }}
+          /> */}
+        </mesh>
+
+        <Sparkles
+          size={6}
+          position-y={1}
+          scale={(4, 2, 4)}
+          speed={0.2}
+          count={30}
+        />
       </Center>
     </>
   );
