06c2f0637a676dfa1a3417e585cdcd50be503d47
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-17 12:24:48 +0100

    save material in state and pass it as property to the mesh and 3d text

--- r3f-3d-text/src/Experience.jsx
+++ r3f-3d-text/src/Experience.jsx
@@ -9,17 +9,20 @@ import { useState } from "react";
 
 export default function Experience() {
   const [torusGeometry, setTorusGeometry] = useState();
+  const [material, setMaterial] = useState();
   const [matCapTexture] = useMatcapTexture("46804D_CBE9AC_90B57C_95D38F", 256);
 
   return (
     <>
       <Perf position="top-left" />
 
-      <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
       <OrbitControls makeDefault />
+      <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matCapTexture} />
 
       <Center>
         <Text3D
+          material={material}
           font="./fonts/helvetiker_regular.typeface.json"
           sie={0.75}
           height={0.2}
@@ -31,13 +34,14 @@ export default function Experience() {
           bevelSize={0.02}
         >
           Hello R3F
-          <meshMatcapMaterial matcap={matCapTexture} />
+          {/* <meshMatcapMaterial matcap={matCapTexture} /> */}
         </Text3D>
       </Center>
       {[...Array(100)].map((value, index) => (
         <mesh
           key={index}
           geometry={torusGeometry}
+          material={material}
           position={[
             (Math.random() - 0.5) * 10,
             (Math.random() - 0.5) * 10,
@@ -45,9 +49,7 @@ export default function Experience() {
           ]}
           scale={0.2 + Math.random() * 0.2}
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        >
-          <meshMatcapMaterial matcap={matCapTexture} />
-        </mesh>
+        />
       ))}
     </>
   );
