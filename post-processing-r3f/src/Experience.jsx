2c231cd9cfb4204ee67f74db74212e3984a2875d
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-18 12:17:19 +0100

    add Glitch effect

--- post-processing-r3f/src/Experience.jsx
+++ post-processing-r3f/src/Experience.jsx
@@ -1,17 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { EffectComposer, Vignette } from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
+import { EffectComposer, Vignette, Glitch } from "@react-three/postprocessing";
+import { BlendFunction, GlitchMode } from "postprocessing";
 
 export default function Experience() {
   return (
     <>
       <color args={["#ffffff"]} attach="background" />
       <EffectComposer multisampling={8}>
-        <Vignette
+        {/* <Vignette
           offset={0.3}
           darkness={0.9}
           blendFunction={BlendFunction.NORMAL}
+        /> */}
+        <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          mode={GlitchMode.CONSTANT_MILD}
         />
       </EffectComposer>
 
