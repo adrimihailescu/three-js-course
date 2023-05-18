67864f489e9a7c078e77c3aa5672e4bc0aa5219f
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-18 16:07:30 +0100

    add basic effect and component Drunk

---
+++ post-processing-r3f/src/Drunk.jsx
@@ -0,0 +1,6 @@
+import DrunkEffect from "./DrunkEffect";
+
+export default function Drunk() {
+  const effect = new DrunkEffect();
+  return <primitive object={effect} />;
+}
--- post-processing-r3f/src/Experience.jsx
+++ post-processing-r3f/src/Experience.jsx
@@ -11,40 +11,41 @@ import {
 } from "@react-three/postprocessing";
 import { BlendFunction, GlitchMode } from "postprocessing";
 import { useControls } from "leva";
+import Drunk from "./Drunk";
 
 //DepthOfField nearly broke my laptop!!! Same as SSR! Bad performance!
 
 export default function Experience() {
-  const ssrProps = useControls("SSR Effect", {
-    temporalResolve: true,
-    STRETCH_MISSED_RAYS: true,
-    USE_MRT: true,
-    USE_NORMALMAP: true,
-    USE_ROUGHNESSMAP: true,
-    ENABLE_JITTERING: true,
-    ENABLE_BLUR: true,
-    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
-    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
-    maxSamples: { value: 0, min: 0, max: 1 },
-    resolutionScale: { value: 1, min: 0, max: 1 },
-    blurMix: { value: 0.5, min: 0, max: 1 },
-    blurKernelSize: { value: 8, min: 0, max: 8 },
-    blurSharpness: { value: 0.5, min: 0, max: 1 },
-    rayStep: { value: 0.3, min: 0, max: 1 },
-    intensity: { value: 1, min: 0, max: 5 },
-    maxRoughness: { value: 0.1, min: 0, max: 1 },
-    jitter: { value: 0.7, min: 0, max: 5 },
-    jitterSpread: { value: 0.45, min: 0, max: 1 },
-    jitterRough: { value: 0.1, min: 0, max: 1 },
-    roughnessFadeOut: { value: 1, min: 0, max: 1 },
-    rayFadeOut: { value: 0, min: 0, max: 1 },
-    MAX_STEPS: { value: 20, min: 0, max: 20 },
-    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
-    maxDepthDifference: { value: 3, min: 0, max: 10 },
-    maxDepth: { value: 1, min: 0, max: 1 },
-    thickness: { value: 10, min: 0, max: 10 },
-    ior: { value: 1.45, min: 0, max: 2 },
-  });
+  // const ssrProps = useControls("SSR Effect", {
+  //   temporalResolve: true,
+  //   STRETCH_MISSED_RAYS: true,
+  //   USE_MRT: true,
+  //   USE_NORMALMAP: true,
+  //   USE_ROUGHNESSMAP: true,
+  //   ENABLE_JITTERING: true,
+  //   ENABLE_BLUR: true,
+  //   temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+  //   temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+  //   maxSamples: { value: 0, min: 0, max: 1 },
+  //   resolutionScale: { value: 1, min: 0, max: 1 },
+  //   blurMix: { value: 0.5, min: 0, max: 1 },
+  //   blurKernelSize: { value: 8, min: 0, max: 8 },
+  //   blurSharpness: { value: 0.5, min: 0, max: 1 },
+  //   rayStep: { value: 0.3, min: 0, max: 1 },
+  //   intensity: { value: 1, min: 0, max: 5 },
+  //   maxRoughness: { value: 0.1, min: 0, max: 1 },
+  //   jitter: { value: 0.7, min: 0, max: 5 },
+  //   jitterSpread: { value: 0.45, min: 0, max: 1 },
+  //   jitterRough: { value: 0.1, min: 0, max: 1 },
+  //   roughnessFadeOut: { value: 1, min: 0, max: 1 },
+  //   rayFadeOut: { value: 0, min: 0, max: 1 },
+  //   MAX_STEPS: { value: 20, min: 0, max: 20 },
+  //   NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+  //   maxDepthDifference: { value: 3, min: 0, max: 10 },
+  //   maxDepth: { value: 1, min: 0, max: 1 },
+  //   thickness: { value: 10, min: 0, max: 10 },
+  //   ior: { value: 1.45, min: 0, max: 2 },
+  // });
   return (
     <>
       <color args={["#ffffff"]} attach="background" />
@@ -67,6 +68,7 @@ export default function Experience() {
         /> */}
         {/* <DepthOfField focusDistance={0.025} focusLength={0.025} />  */}
         {/* <SSR {...ssrProps} /> */}
+        <Drunk />
       </EffectComposer>
 
       <Perf position="top-left" />
