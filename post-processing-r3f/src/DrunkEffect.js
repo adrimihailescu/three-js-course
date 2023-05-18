1356d9890616c476764b565136226e038f9907bf
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-18 17:46:20 +0100

    add unifor offset and update it on each frame

--- post-processing-r3f/src/DrunkEffect.js
+++ post-processing-r3f/src/DrunkEffect.js
@@ -4,8 +4,9 @@ import { Uniform } from "three";
 const fragmentShader = `
 uniform float frequency;
 uniform float amplitude;
+uniform float offset;
 void mainUv(inout vec2 uv) {
-  uv.y += sin(uv.x * frequency) * amplitude;
+  uv.y += sin(uv.x * frequency + offset) * amplitude;
 }
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
 {
@@ -20,7 +21,11 @@ export default class DrunkEffect extends Effect {
       uniforms: new Map([
         ["frequency", new Uniform(frequency)],
         ["amplitude", new Uniform(amplitude)],
+        ["offset", new Uniform(0)],
       ]),
     });
+  }
+  update(renderer, inputBuffer, deltaTime) {
+    this.uniforms.get("offset").value += deltaTime;
   }
 }
