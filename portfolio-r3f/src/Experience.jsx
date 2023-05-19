a9842a0280e384ef26a7d32cb7568560d9ad5599
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-19 15:18:21 +0100

    add computer to the scene with gltf loader hook from cdn, add float around computer, remove cube mesh

--- portfolio-r3f/src/Experience.jsx
+++ portfolio-r3f/src/Experience.jsx
@@ -1,15 +1,18 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, Environment, Float } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
-
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+  //   console.log(computer);
+  return (
+    <>
+      <Environment preset="city" />
+      <color args={["#241a1a"]} attach="background" />
+      <OrbitControls makeDefault />
+      <Float rotationIntensity={0.4}>
+        <primitive object={computer.scene} position-y={-1.2} />
+      </Float>
     </>
+  );
 }
