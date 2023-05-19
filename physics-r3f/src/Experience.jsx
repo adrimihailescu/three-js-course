c58448abcb3489be5f484bbc41a0e93244b72161
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-19 18:03:42 +0100

    add useRef to get access to the cube mesh and pass on cubeJump fundtion

--- physics-r3f/src/Experience.jsx
+++ physics-r3f/src/Experience.jsx
@@ -1,4 +1,5 @@
 import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 import { Perf } from "r3f-perf";
 import {
   Debug,
@@ -9,6 +10,17 @@ import {
 } from "@react-three/rapier";
 
 export default function Experience() {
+  const cubeRef = useRef();
+
+  const cubeJump = () => {
+    // console.log("jump");
+    cubeRef.current.applyImpulse({ x: 0, y: 5, z: 0 }); //this will make the cube jump on the y axis
+    cubeRef.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
   return (
     <>
       <Perf position="top-left" />
@@ -20,20 +32,26 @@ export default function Experience() {
       <Physics>
         <Debug />
         <RigidBody colliders="ball">
-          <mesh castShadow position={[0, 4, 0]}>
+          <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
-        <RigidBody
+        {/* <RigidBody
           colliders={false}
           position={[0, 1, 0]}
           rotation={[Math.PI * 0.5, 0, 0]}
-        >
-          {/* <CuboidCollider args={[1.5, 1.5, 0.5]} /> */}
-          <BallCollider args={[1.5]} />
+        > */}
+        {/* <CuboidCollider args={[1.5, 1.5, 0.5]} /> */}
+        {/* <BallCollider args={[1.5]} />
           <mesh castShadow>
             <torusGeometry args={[1, 0.5, 15, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
+        <RigidBody position={[1.5, 2, 0]} ref={cubeRef}>
+          <mesh castShadow onClick={cubeJump}>
+            <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody>
