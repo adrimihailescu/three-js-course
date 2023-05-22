1d24e31255ec82d37aa47ac4e74aa9c1bc4b1c22
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-22 13:56:21 +0100

    add component BlockSPinner and animation for the obstacle

--- marble-race-game-r3f/src/Level.jsx
+++ marble-race-game-r3f/src/Level.jsx
@@ -1,4 +1,7 @@
+import { RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
 
 //deactivate legacyMode on Three.js to set the same encoding for R3F
 THREE.ColorManagement.legacyMode = false;
@@ -27,11 +30,58 @@ function BlockStart({ position = [0, 0, 0] }) {
     </group>
   );
 }
+function BLockSpinner({ position = [0, 0, 0] }) {
+  //create reference to the obstacle we want to animate
+  const obstacle = useRef();
+  //save speed in state
+  const [speed] = useState(
+    () => Math.random() + 0.2 //prevent the speed from being too slow
+  );
+
+  //update the animation on each frame
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const rotation = new THREE.Quaternion();
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
+    obstacle.current.setNextKinematicRotation(rotation);
+  });
+  return (
+    <group position={position}>
+      {/**
+       * Floor
+       */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor2Material}
+        position={[0, -0.1, 0]}
+        receiveShadow
+        scale={[4, 0.2, 4]}
+      />
+      <RigidBody
+        ref={obstacle}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
 
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 4]} />
+      <BLockSpinner position={[0, 0, 0]} />
     </>
   );
 }
