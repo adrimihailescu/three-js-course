2a3b5e0ccfbe92ac5ab725a1ee80422f4f098734
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-22 18:48:34 +0100

    add keyboard controld and make them available im component Player, add interaction with the ball to move by key press

--- marble-race-game-r3f/src/Player.jsx
+++ marble-race-game-r3f/src/Player.jsx
@@ -1,9 +1,49 @@
 import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Player() {
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  //create reference to the ball/marble so we can interact with it
+  const ball = useRef();
+  useFrame((state, delta) => {
+    //get the keys
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 1 * delta;
+    const torqueStrength = 1 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    ball.current.applyImpulse(impulse);
+    ball.current.applyTorqueImpulse(torque);
+  });
   return (
     <>
       <RigidBody
+        ref={ball}
         position={[0, 1, 0]}
         colliders="ball"
         friction={1}
