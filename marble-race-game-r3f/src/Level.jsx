dd8ee65ff07bc09f39deeb05e301625997b7decd
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-22 12:55:15 +0100

    add box geometry and materials outside of component for all objects

--- marble-race-game-r3f/src/Level.jsx
+++ marble-race-game-r3f/src/Level.jsx
@@ -1,16 +1,37 @@
-function BlockStart() {
+import * as THREE from "three";
+
+//deactivate legacyMode on Three.js to set the same encoding for R3F
+THREE.ColorManagement.legacyMode = false;
+
+//create geometry outside of the component
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+//create material outside of the component
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+function BlockStart({ position = [0, 0, 0] }) {
   return (
-    <mesh position={[0, -0.1, 0]} receiveShadow>
-      <boxGeometry args={[4, 0.2, 4]} />
-      <meshStandardMaterial color="limegreen" />
-    </mesh>
+    <group position={position}>
+      {/**
+       * Floor
+       */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        receiveShadow
+        scale={[4, 0.2, 4]}
+      />
+    </group>
   );
 }
 
 export default function Level() {
   return (
     <>
-      <BlockStart />
+      <BlockStart position={[0, 0, 0]} />
     </>
   );
 }
