25d9134f23e1c24895a61186d034d82dc4323f88
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-23 16:48:00 +0100

    add zustant for state management and create store for blocks count

--- marble-race-game-r3f/src/Experience.jsx
+++ marble-race-game-r3f/src/Experience.jsx
@@ -3,15 +3,19 @@ import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import { Physics, Debug } from "@react-three/rapier";
 import Player from "./Player.jsx";
+import useGame from "./stores/useGame.js";
 
 export default function Experience() {
+  const blocksCount = useGame((state) => {
+    return state.blocksCount;
+  });
   return (
     <>
       {/* <OrbitControls makeDefault /> */}
       <Physics>
         {/* <Debug /> */}
         <Lights />
-        <Level />
+        <Level count={blocksCount} />
         <Player />
       </Physics>
     </>
