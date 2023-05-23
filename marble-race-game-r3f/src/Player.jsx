21d6ae52605f616b5b5874cf551f54f40288b868
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-23 10:14:11 +0100

    add jump functionality to the marble

--- marble-race-game-r3f/src/Player.jsx
+++ marble-race-game-r3f/src/Player.jsx
@@ -1,12 +1,32 @@
 import { RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 export default function Player() {
   const [subscribeKeys, getKeys] = useKeyboardControls();
   //create reference to the ball/marble so we can interact with it
   const ball = useRef();
+
+  const jump = () => {
+    //calculate the origin of the ball or how hight it is from the floor
+    const origin = ball.current.translation();
+    origin.y -= 0.31;
+    //
+
+    // console.log("yes,Jump!");
+    ball.current.applyImpulse({ x: 0, y: 0.5, z: 0 }); //make the ball jump
+  };
+  useEffect(() => {
+    subscribeKeys(
+      (state) => state.jump,
+      (value) => {
+        if (value) {
+          jump();
+        }
+      }
+    );
+  }, []);
   useFrame((state, delta) => {
     //get the keys
     const { forward, backward, leftward, rightward } = getKeys();
@@ -46,6 +66,8 @@ export default function Player() {
         ref={ball}
         position={[0, 1, 0]}
         colliders="ball"
+        linearDamping={0.5} // this will help the ball to stop and easier to control with the keyboard
+        angularDamping={0.5}
         friction={1}
         restitution={0.2}
       >
